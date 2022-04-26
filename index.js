5cdba57cd248e8538d7ccbfc03fdbe4ff6576ad9
= <maria.louisa.castro@gmail.com>
2022-04-26 22:04:15 +0100

    Step in pseudo

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 // Get the input typed and add it to the ol
 // Add a scroll on the list of longer than page so input is always at top
+// Add an array with all 195 countries
+// Add functionality to check button (check list against array)
