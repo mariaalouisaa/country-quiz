616eb0fdc5f09d5662664139a52767da70c7f908
= <maria.louisa.castro@gmail.com>
2022-04-26 22:08:06 +0100

    Add event to submit

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+const addItem = (e) => {
+  e.preventDefault();
+  console.log("add an item");
+};
+
+document.querySelector("#submit").addEventListener("click", addItem);
+
 // Get the input typed and add it to the ol
 // Add a scroll on the list of longer than page so input is always at top
 // Add an array with all 195 countries
